27011105b6da0c409f64fae32442219d29647c20
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-06-22 23:21:38 -0300

    Add fetching static products data from the backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import {
   Row,
@@ -9,13 +9,23 @@ import {
   Card,
   ListGroupItem,
 } from 'react-bootstrap';
+import axios from 'axios';
 
 import Rating from '../components/Rating';
-import products from '../products';
 
 const ProductScreen = () => {
   let { id } = useParams();
-  const product = products.find((p) => p._id === id);
+
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data);
+    }
+    fetchProduct();
+  }, []);
+
   return (
     <div>
       <Link to='/' className='btn btn-light my-3'>
