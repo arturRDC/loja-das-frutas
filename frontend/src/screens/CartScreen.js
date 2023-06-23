d74a59c5b7df1fef087e92b781e3d3aefbd2ae6b
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-06-23 18:43:44 -0300

    Trigger add to cart action on button click

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useParams, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Form,
+  Image,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import { Message } from '../components/Message';
+import { addToCart } from '../actions/cartActions';
 
 const CartScreen = () => {
+  const { id } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty));
+    }
+  }, [dispatch, id, qty]);
+
   return <div>CartScreen</div>;
 };
 
