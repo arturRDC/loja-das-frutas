192745516aa6e9365c0c550cced3064a016a85ea
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-06-23 21:22:56 -0300

    Add all cart functionality, except for final purchase

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -9,9 +9,10 @@ import {
   Image,
   Button,
   Card,
+  ListGroupItem,
 } from 'react-bootstrap';
-import { Message } from '../components/Message';
-import { addToCart } from '../actions/cartActions';
+import Message from '../components/Message';
+import { addToCart, deleteFromCart } from '../actions/cartActions';
 
 const CartScreen = () => {
   const { id } = useParams();
@@ -30,7 +31,94 @@ const CartScreen = () => {
     }
   }, [dispatch, id, qty]);
 
-  return <div>CartScreen</div>;
+  function handleDeleteFromCart(id) {
+    dispatch(deleteFromCart(id));
+  }
+  function handleCheckout() {
+    navigate('/login?redirect=shipping');
+  }
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Carrinho de compras</h1>
+        {cartItems.length === 0 ? (
+          <Message variant='info'>
+            O seu carrinho está vazio.{' '}
+            <Link to='/'>Clique aqui para voltar para a página principal</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} rounded fluid />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={3}>
+                    <Form.Control
+                      as='select'
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.stockCount).keys()].map((x) => (
+                        <option value={x + 1} key={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={1}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => handleDeleteFromCart(item.product)}
+                    >
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                {cartItems.reduce((acc, item) => acc + item.qty, 0) + ' '}
+                produto(s) no seu carrinho
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                disabled={cartItems.length === 0}
+                onClick={handleCheckout}
+              >
+                Comprar
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default CartScreen;
