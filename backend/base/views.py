792478e75de5f573eb192c83eb182b4f9579c740
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-06-24 09:13:13 -0300

    Protect users route from non-admin users

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from .models import Product
+from django.contrib.auth.models import User
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
@@ -37,9 +39,17 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 @api_view(["GET"])
@@ -54,3 +64,17 @@ def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
